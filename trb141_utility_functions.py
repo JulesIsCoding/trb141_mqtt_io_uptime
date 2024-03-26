0e30fd48811009acb5d7b5a9c427c4fbc175baad
JulesIsCoding <156131413+JulesIsCoding@users.noreply.github.com>
2024-03-26 13:48:13 +1300

    update mqtt subscribe utils

--- trb141_utility_functions.py
+++ trb141_utility_functions.py
@@ -10,68 +10,64 @@ def command(info_logger, error_logger, payload, thread_manager):
         f"[{current_time}] Received message from subscribed topic: {str(payload)}"
     )
 
-    # Extract the 'message' dictionary from the payload
-    message = payload.get('message')
+    cmd = payload.get('command')
 
-    if message:  # Ensure 'message' dictionary exists
-        cmd = message.get('command')
+    if cmd == "set_output":
+        output = payload['data']['gpio']
+        value = payload['data']['value']
+        set_output(output, value)
 
-        if cmd == "set_output":
-            output = message['data']['gpio']
-            value = message['data']['value']
-            set_output(output, value)
-
-        elif cmd == "set_uptime":
-            name = message['data']['name']
-            value = message['data']['value']
+    elif cmd == "set_uptime":
+        name = payload['data']['name']
+        value = payload['data']['value']
+        try:
+            thread_manager.stop_gpio_thread()
+            reading = {
+                "name": name,
+                "numericValue": value,
+                "timestamp": time.time()
+            }
             try:
-                thread_manager.stop_gpio_thread()
-                reading = {
-                    "name": name,
-                    "numericValue": value,
-                    "timestamp": time.time()
-                }
+                trb141_db.insert_or_update_persistent_data(
+                    reading, error_logger
+                )
                 try:
-                    trb141_db.insert_or_update_persistent_data(
-                        reading, error_logger
-                    )
-                    try:
-                        # Device authentication details.
-                        SERIAL_NUMBER = trb141_api.get_serial_number()
-                        thread_manager.start_gpio_thread(SERIAL_NUMBER)
-                    except Exception as e:
-                        current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-                        error_logger.error(
-                            f"[{current_time}] Error starting runtime: {e}", exc_info=True
-                        )
+                    # Device authentication details.
+                    SERIAL_NUMBER = trb141_api.get_serial_number()
+                    thread_manager.start_gpio_thread(SERIAL_NUMBER)
                 except Exception as e:
                     current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
                     error_logger.error(
-                        f"[{current_time}] Error inserting or updating persistent data: {e}"
+                        f"[{current_time}] Error starting runtime: {e}", exc_info=True
                     )
             except Exception as e:
                 current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
                 error_logger.error(
-                    f"[{current_time}] Error stopping runtime: {e}", exc_info=True
+                    f"[{current_time}] Error inserting or updating persistent data: {e}"
                 )
-
-        elif cmd == "send_report":
-            try:
-                # Device authentication details.
-                SERIAL_NUMBER = trb141_api.get_serial_number()
-                thread_manager.restart_gpio_thread(SERIAL_NUMBER)
-            except Exception as e:
-                current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-                error_logger.error(
-                    f"[{current_time}] Error restarting runtime: {e}", exc_info=True
-                )
-
-        elif cmd == "update_runtime":
-            update_runtime(info_logger, error_logger)
-
-        else:
+        except Exception as e:
             current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-            info_logger.info(f"[{current_time}] Unsupported method")
+            error_logger.error(
+                f"[{current_time}] Error stopping runtime: {e}", exc_info=True
+            )
+
+    elif cmd == "send_report":
+        try:
+            # Device authentication details.
+            SERIAL_NUMBER = trb141_api.get_serial_number()
+            thread_manager.restart_gpio_thread(SERIAL_NUMBER)
+        except Exception as e:
+            current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+            error_logger.error(
+                f"[{current_time}] Error restarting runtime: {e}", exc_info=True
+            )
+
+    elif cmd == "update_runtime":
+        update_runtime(info_logger, error_logger)
+
+    else:
+        current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        info_logger.info(f"[{current_time}] Unsupported method")
 
 def set_output(output, value):
     # When one Relay output is open, the other one is closed; so to turn an output on or off, we have to change the value on both pins:
